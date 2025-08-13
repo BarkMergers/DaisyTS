ffcd0e4583046461c761bb8c45636ba39318cdd8
Mark Burgess <mburgess1983@gmail.com>
2025-08-13 12:19:42 +0100

    d

--- daisyts2/tailwind.config.js
+++ daisyts2/tailwind.config.js
@@ -1,4 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  safelist: ['w-12', 'w-16'],
-}
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    safelist: ['w-12', 'w-16'],
+    theme: {
+        extend: {},
+    },
+    plugins: [require("daisyui")],
+};
